0c25cdbd56ffff9be35c7accf6d80f2edd5e2e7a
SergioFelzener <felzenerdj@gmail.com>
2023-01-02 09:01:09 -0500

    finalizando tipos basicos

---
+++ fundamentos/teste.py
@@ -0,0 +1,7 @@
+print("teste")
+a = 1
+b = [1, 2, 3]
+c = {"ana", "Luiz", "teste"}
+d = ("isso", "tupla")
+
+print(a, b, c, d)
---
+++ fundamentos/tipos_basicos.py
@@ -0,0 +1,10 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print("Aqui eu falo minha lingua")
+print('Também funciona aspas simples')
+print("voce é" + 3 * 'muito ' + "legal")
+# print(3 + '3') -> não funciona pois é ambiguo
+print({1, 2, 3})  # -> lista
+print({"nome": "ana", "idade": 22})
+print(None)
