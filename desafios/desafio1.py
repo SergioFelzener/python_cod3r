702842a87ac50ab4cecb72ad09a8238cc98d4b6b
SergioFelzener <felzenerdj@gmail.com>
2023-01-02 10:19:52 -0500

    finalizando desafio

---
+++ desafios/desafio1.py
@@ -0,0 +1,15 @@
+# quanto é o percentual de comprometimento das
+# despesas em relação ao salário
+
+salario = 3450.45
+despesas = 2456.2
+
+calculo_despesa = despesas * 100
+print(calculo_despesa)
+
+percentual = calculo_despesa / salario
+
+# outra solução
+
+percentual_comprometido = despesas / salario * 100
+print(percentual_comprometido)
