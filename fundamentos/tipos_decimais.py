4437454d1d4c94ca637533bbdf508fa0e449ec54
SergioFelzener <felzenerdj@gmail.com>
2023-01-02 13:55:19 -0500

    finalizando números completo

---
+++ fundamentos/tipos_decimais.py
@@ -0,0 +1,20 @@
+from decimal import Decimal, getcontext
+
+print(1.1 + 2.2)  # resultado em 3.3000000000000003
+
+print(Decimal(1) / Decimal(7))  # resultado 0.1428571428571428571428571429
+getcontext().prec = 4  # mudando precisao de casa decimal
+print(Decimal(1) / Decimal(7))  # resultado 0.1429
+
+print(Decimal.max(Decimal(1), Decimal(7)))  # printa maior decimal entre eles
+
+print("o que está disponível dentro de Decimal")
+print(" \n*************** dir(Decimal) *****************\n")
+print(dir(Decimal))
+
+print(" \n*************** dir(Decimal) *****************\n")
+
+print(Decimal(1.1) + Decimal(2.2))
+print(" \n******** Verificação de disponibilidade global **********\n")
+print(" \n*************** dir() *****************\n")
+print(dir())
---
+++ fundamentos/tipos_numericos.py
@@ -0,0 +1,19 @@
+print("\n******************** dir(int) *****************\n")
+print(dir(int))
+print("\n******************** dir(float) *****************\n")
+print(dir(float))
+
+print("\n******************** Pratica *****************\n")
+
+a = 5
+b = 2.5
+
+print(a / b)
+print(a + b)
+print(a - b)
+print(type(a))
+print(type(b))
+
+print(b.is_integer())
+print(5.0.is_integer())
+print(5.1.is_integer())
