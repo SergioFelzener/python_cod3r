faaafc036179ecb6bb99b1d0c98bc9a15e68a771
SergioFelzener <felzenerdj@gmail.com>
2023-01-02 09:11:22 -0500

    finalizando variáveis

---
+++ fundamentos/variaveis.py
@@ -0,0 +1,11 @@
+# variavéis
+
+a = 10  # inteiro
+b = 5.2  # float
+
+print(a + b)
+
+a = "agora sou uma string"
+print(a)
+# print(a + b) -> não funciona pois está somando uma string com um numero
+
