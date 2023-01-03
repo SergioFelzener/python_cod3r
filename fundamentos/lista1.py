6b7cad27b893c98d1f5068611b600a95da63c423
SergioFelzener <felzenerdj@gmail.com>
2023-01-03 14:34:01 -0500

    finalizando lista parte 1

---
+++ fundamentos/lista1.py
@@ -0,0 +1,17 @@
+lista = []
+# print(type(lista))
+# print(dir(lista))
+# print(help(list))
+
+print(len(lista))
+lista.append(1)
+lista.append(5)
+print(lista)
+print(len(lista))
+nova_lista = [1, 5, "Ana", "Paulo"]
+print(nova_lista)
+nova_lista.remove(5)
+print(nova_lista)
+nova_lista.reverse()
+print(nova_lista)
+print()
