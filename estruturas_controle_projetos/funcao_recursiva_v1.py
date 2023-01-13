d3ce37a20cf9688d9f3d4d41b9b607e94217d769
SergioFelzener <felzenerdj@gmail.com>
2023-01-12 20:02:27 -0500

    update

--- estruturas_controle_projetos/funcao_recursiva_v1.py
+++ estruturas_controle_projetos/funcao_recursiva_v1.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-# sequencia [0, 1] = 1  / [1, 1] = 2 / [1, 2] = 3 / [2, 3] = 5 / [3, 5] = 8 / [5, 8] = 13 / [8, 13] = 21 / [13, 21] = 34 / [21, 34] = 55 / .....
-# 0, 1, 1, 2, 3, 5, 8, 13, 21 .....
 
+# função recursiva que chama ela mesma
 def imprimir(maximo, atual):
     if atual >= maximo:
         return
     print(atual)
     imprimir(imprimir(maximo, atual + 1))
+
 
 imprimir(990, 1)
