02d16ac99cd01c269f789612db9a2e77982bec27
SergioFelzener <felzenerdj@gmail.com>
2023-01-28 18:16:04 -0500

    salvando progresso

--- game/pokemonpy/pokemon.py
+++ game/pokemonpy/pokemon.py
@@ -1,26 +1,28 @@
 class Pokemon:
-    def __init__(self, tipo, especie, power=100, magic=0):
+    def __init__(self, tipo, especie, name=None, power=100, magic=0, level=1):
         self.tipo = tipo
         self.especie = especie
+        self.name = name
         self.power = power
         self.magic = magic
-        
+        self.level = level
+
+        if name:
+            self.name = name
+        else:
+            self.name = especie
+
     def __str__(self):
-        # return f'Pokemon {self.especie, self.tipo, self.power}'
-        return 'Especie : {}\nTipo : {}\nPower = {}\n'.format(self.especie, self.tipo, self.power)
-        
+        return f'Pokemon Nome **{self.name}**\nSua espécie é : {self.especie}\nSeu tipo : {self.tipo}\nForça {self.power}'
+        # return 'Especie : {}\nTipo : {}\nPower = {}\n'.format(self.especie, self.tipo, self.power)
+
     def attack(self, pokemon):
         print("{}\nAtacou .....{} ".format(self, pokemon))
         pokemon.power = pokemon.power - 10
         print(pokemon)
-        
-        
-        
-        
 
 
 p1 = Pokemon('Fogo', 'Chamamix')
 p2 = Pokemon('Eletrico', 'Pikachu')
+print(p1)
 p1.attack(p2)
-
-
