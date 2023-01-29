af5c42ddf98abb5fa46cd1e73a357b93f61850ce
SergioFelzener <felzenerdj@gmail.com>
2023-01-29 14:58:16 -0500

    salvando update 3

--- game/pokemonpy/pokemon.py
+++ game/pokemonpy/pokemon.py
@@ -1,6 +1,5 @@
 class Pokemon:
-    def __init__(self, tipo, especie, name=None, power=100, magic=0, level=1):
-        self.tipo = tipo
+    def __init__(self, especie, name=None, power=100, magic=0, level=1):
         self.especie = especie
         self.name = name
         self.power = power
@@ -13,7 +12,7 @@ class Pokemon:
             self.name = especie
 
     def __str__(self):
-        return f'Pokemon Nome **{self.name}**\nSua espécie é : {self.especie}\nSeu tipo : {self.tipo}\nForça {self.power}\nMagia : {self.magic}\nLevel : {self.level}'
+        return f'Pokemon Nome **{self.name}**\nSua espécie é : {self.especie}\nForça {self.power}\nMagia : {self.magic}\nLevel : {self.level}'
         # return 'Especie : {}\nTipo : {}\nPower = {}\n'.format(self.especie, self.tipo, self.power)
 
     def attack(self, pokemon):
@@ -24,7 +23,67 @@ class Pokemon:
         print('Seu força caiu para', pokemon.power)
 
 
-p1 = Pokemon('Fogo', 'Chamamix')
-p2 = Pokemon('Eletrico', 'Pikachu')
+class PokemonEletrico(Pokemon):
+    tipo = 'Eletrico'
+
+    def attack(self, pokemon):
+        attack = 20
+        self.level += 1
+        print('{} seu Level subiu, novo Level {}'.format(self.name, self.level))
+        print(self)
+        pokemon.power = pokemon.power - attack
+        print('{} Lançou um Raio no {}'.format(self.name, pokemon))
+
+    def choque220(self, pokemon):
+        xoq_attack = 50
+        pokemon.power -= xoq_attack
+        print("{} Deu choque 220 , fritou {}".format(self.name, pokemon))
+        print('{} danificou seu inimigo {}'.format(self.name, xoq_attack))
+
+
+class PokemonFogo(Pokemon):
+    tipo = 'Fogo'
+    
+    def attack(self, pokemon):
+        attack = 20
+        self.level += 1
+        print('{} seu Level subiu, novo Level {}'.format(self.name, self.level))
+        print(self)
+        pokemon.power = pokemon.power - attack
+        print('{} Lançou Uma bola de FOGO => no {}'.format(self.name, pokemon))
+
+
+class PokemonAgua(Pokemon):
+    tipo = 'Agua'
+    
+    def attack(self, pokemon):
+        attack = 20
+        self.level += 1
+        print('{} seu Level subiu, novo Level {}'.format(self.name, self.level))
+        print(self)
+        pokemon.power = pokemon.power - attack
+        print('{} Lançou Um Jato de ÁGUA => no {}'.format(self.name, pokemon))
+
+
+p1 = Pokemon('Chamamix', name='ZOARK')
+p2 = Pokemon('Pikachu')
 print(p1)
 p1.attack(p2)
+print("pokemon eletrico criando")
+print("**********************************")
+print("")
+p3 = PokemonEletrico("pikachu", name="JAIR")
+print(p3)
+print("")
+print("**********************************")
+print("")
+p3.attack(p1)
+print(p1)
+print("")
+print("**********************************")
+print("")
+p3.choque220(p1)
+
+p4 = PokemonAgua('Torneirinha', name='TORNERA')
+print(p4)
+
