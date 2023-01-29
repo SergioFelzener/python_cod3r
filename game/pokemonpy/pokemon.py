6bea20627cc3381d6671fa962fc3f8d34df0a234
SergioFelzener <felzenerdj@gmail.com>
2023-01-29 13:51:02 -0500

    salvando update

--- game/pokemonpy/pokemon.py
+++ game/pokemonpy/pokemon.py
@@ -13,13 +13,15 @@ class Pokemon:
             self.name = especie
 
     def __str__(self):
-        return f'Pokemon Nome **{self.name}**\nSua espécie é : {self.especie}\nSeu tipo : {self.tipo}\nForça {self.power}'
+        return f'Pokemon Nome **{self.name}**\nSua espécie é : {self.especie}\nSeu tipo : {self.tipo}\nForça {self.power}\nMagia : {self.magic}\nLevel : {self.level}'
         # return 'Especie : {}\nTipo : {}\nPower = {}\n'.format(self.especie, self.tipo, self.power)
 
     def attack(self, pokemon):
+        attack = 10
         print("{}\nAtacou .....{} ".format(self, pokemon))
-        pokemon.power = pokemon.power - 10
-        print(pokemon)
+        pokemon.power = pokemon.power - attack
+        print(f'sua força de ataque foi ... {attack}')
+        print('Seu força caiu para', pokemon.power)
 
 
 p1 = Pokemon('Fogo', 'Chamamix')
