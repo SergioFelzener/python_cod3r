ea2b5f2b55f17d0521d76ec9465f05a7ad360681
SergioFelzener <felzenerdj@gmail.com>
2023-02-11 14:57:23 -0500

    herança multipla exemplo

---
+++ POOA/mixins.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+class HtmlToStringMixin:
+    def __str__(self):
+        # conversão para html
+        html = super().__str__() \
+            .replace('(', '<strong>(') \
+            .replace(')', ')</strong>')
+        return f'<span>{html}</span>'
+    
+    
+class Pessoa:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def __str__(self):
+        return self.nome
+    
+
+class Animal:
+    def __init__(self, nome, pet=True):
+        self.nome = nome
+        self.pet = pet
+        
+    def __str__(self):
+        return self.nome + '({pet})' if self.pet else ''
+
+
+class PessoaHtml(HtmlToStringMixin, Pessoa):
+    pass
+
+class AnimalHtml(HtmlToStringMixin, Animal):
+    pass
+
+if __name__ == '__main__':
+    p1 = Pessoa('Maria Eduarda')
+    print(p1)
+    p2 = PessoaHtml('Luiz Augusto')
+    print(p2)
+    a1 = Animal('Pato')
+    print(a1)
+    a2 = AnimalHtml('Totó')
+    print(a2)
+    
+    
+        
+            
